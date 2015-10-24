52f51610307a2f4fbb22efb56601b3ca9da2db28
klauszi <klausz1993@hotmail.de>
2015-10-24 10:51:58 +0200

    Aufgabe2-fast-fertig

--- 02-gdb-valgrind/02-gdb-valgrind/broken-pde/partdiff-seq.c
+++ 02-gdb-valgrind/02-gdb-valgrind/broken-pde/partdiff-seq.c
@@ -93,7 +93,7 @@ allocateMatrices (void)
       errorQuit ();
     }				/* quit if error   */
 
-  M = malloc (sizeof (double) * (N + 1) * (N - 1) * 2);	/* allocate memory */
+  M = malloc (sizeof (double) * (N + 1) * (N - 1) * 2 * 2);	/* allocate memory */
   if (M == 0)
     {
       errorQuit ();
@@ -162,11 +162,12 @@ initMatrices (void)
 void
 freeMatrices (void)
 {
-  free (Matrix);
   if (Matrix[1] != 0)
     free (Matrix[1]);
   if (Matrix[0] != 0)
     free (Matrix[0]);
+  free (Matrix);
+  free(M);
 }
 
 
@@ -227,7 +228,7 @@ calculate (void)
 	  for (i = 1; i < N; i++)	/* over all rows  */
 	    {
 	      star = -Matrix[m2][i - 1][j]
-		- Matrix[j - 1][m2][i] + 4 * Matrix[m2][i][j] -
+		- Matrix[m2][i][j-1] + 4 * Matrix[m2][i][j] -
 		Matrix[m2][i][j + 1] - Matrix[m2][i + 1][j];
 
 	      residuum = getResiduum (i, j);
