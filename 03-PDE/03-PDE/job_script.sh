19067eef18a4092007b05af6cbec79dbf93f42d1
theCalcaholic <tobias@knoeppler.net>
2015-10-31 20:56:49 +0100

    Found correct parameters for job_script. Finally!

--- 03-PDE/03-PDE/job_script.sh
+++ 03-PDE/03-PDE/job_script.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-#SBATCH --job_name=HLR_jobscript
+##SBATCH --job_name=HLR_jobscript
 #SBATCH -o timescript.out
 #SBATCH -e jobscript.err
-#SBATCH --tasks-per-node=4
-##SBATCH -n 4
 #SBACH -N 4
+#SBATCH -n 16
+#SBATCH --tasks-per-node=4
 
-srun timescript.sh
-srun timescript.sh
-srun timescript.sh
 srun timescript.sh
 echo fertig
