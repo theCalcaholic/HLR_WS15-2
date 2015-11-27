ecd07c6f2fbbc22ec3ad90dac736c27467555bc9
klauszi <klausz1993@hotmail.de>
2015-11-27 02:16:06 +0100

    [clean up] circle/main - you can now circle one step. But with 10 processes and 21 Numbers it doesn't work...

--- 07-circle/circle.c
+++ 07-circle/circle.c
@@ -4,26 +4,23 @@
 #include <mpi.h>
 
 void
-print_rand_array(char* text, int rank, int number_of_rand, int size,int* buf)
+print_rand_array(char* text, int rank,int number,int store,int size,int* buf)
 {
 
   if(rank == 0)
   {
   	printf("\n%s\n",text);
 	
-	for(int k = 0; k < number_of_rand;k++)
+	for(int k = 0; buf[k] < 25 ;k++)
 	{
 		printf("rank %d: %d\n", rank, buf[k]);
 	}
   	for (int i = 1; i < size; i++)
   	{
-	  int number;
-	  MPI_Recv (&number,1,MPI_INT,i,1,MPI_COMM_WORLD,NULL);
+	  int randmessage[store+1];
+	  MPI_Recv (randmessage,number+1,MPI_INT,i,100,MPI_COMM_WORLD,NULL);
 
-	  int randmessage[number];
-	  MPI_Recv (randmessage,number,MPI_INT,i,100,MPI_COMM_WORLD,NULL);
-
-	  for(int l = 0; l < number; l++)
+	  for(int l = 0;randmessage[l] < 25; l++)
 	  {
     	    	printf ("rank %d: %d\n", i, randmessage[l]);
 	  }
@@ -31,34 +28,35 @@ print_rand_array(char* text, int rank, int number_of_rand, int size,int* buf)
   }
   else
   {
-	MPI_Send(&number_of_rand, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-	MPI_Send(buf, number_of_rand, MPI_INT, 0, 100, MPI_COMM_WORLD);	
+	MPI_Send(buf, number+1, MPI_INT, 0, 100, MPI_COMM_WORLD);	
   }
 }
 
 int*
-init (int number, int rank)
+init (int number,int store, int rank)
 {
   //todo
-  int* buf = malloc(sizeof(int)* number);		//Es wird Speicher für die neuen Zufallszahlen reserviert.
+  int* buf = malloc(sizeof(int)* (store + 1));		//Es wird Speicher für die neuen Zufallszahlen reserviert.
 
   srand(time(NULL)+ rank);				//Prozesse erhalten ohne rank das Selbe.
-
-  for (int i = 0; i < number; i++)
+  
+  int i;
+  for (i = 0; i < number; i++)
   {
     buf[i] = rand() % 25; //do not modify %25
   }
-
+  buf[i] = 25;					//25 ist das Ende der Zufallszahlen (Es darf ja keine 25 geben).
   return buf;
 }
 
-void
-circle (int* buf,int number,int rank,int predecessor, int successor, int size)
+void*
+circle (int* buf,int rank,int predecessor, int successor, int size, int store)
 {
 	
-  int* bufcpy = malloc(sizeof(int)* number);						//Eine Copy des buf für Prozess size-1
-  int numbercpy = number;								//Ein Copy für die Anzahl der Zufallszahlen für size-1
+  int* newbuf = malloc(sizeof(int) * (store + 1)); 					//Eine Copy des buf 
   int special_integer;									//Die Integerzahl, worauf der letzte Prozess wartet.
+  
+  //Übergabe des special_integer an den letzten Prozess 
   if(rank == size-1)
   {
 	 int* special_integer_buf = malloc(sizeof(int));				//Zeiger für den Erhalt der special_integer
@@ -67,8 +65,22 @@ circle (int* buf,int number,int rank,int predecessor, int successor, int size)
   }
   if(rank == 0)
   {
-	MPI_Send(&buf[0], 1, MPI_INT, predecessor, 11, MPI_COMM_WORLD);			//Senden der speziellen Zahl
+	MPI_Send(&newbuf[0], 1, MPI_INT, predecessor, 11, MPI_COMM_WORLD);		//Senden der speziellen Zahl
+  }
+  
+ //Rotieren der Zufallszahlen.
+  if(rank != 0)
+  { 
+	MPI_Recv(newbuf,store + 1,MPI_INT,predecessor,10,MPI_COMM_WORLD,NULL);
+	MPI_Send(buf,store + 1,MPI_INT,successor,10,MPI_COMM_WORLD);
+  }
+  else
+  {
+	MPI_Send(buf,store + 1,MPI_INT,successor,10,MPI_COMM_WORLD);
+	MPI_Recv(newbuf,store + 1,MPI_INT,predecessor,10,MPI_COMM_WORLD,NULL);
   } 
+
+  return newbuf;
 }
 
 int
@@ -83,6 +95,7 @@ main (int argc, char** argv)
   int predecessor;				//Die Nummer des Vorgängers
   int size; 					//Die Anzahl der Prozesse
   int number_of_rand; 				//Anzahl der Zufallszahlen, die der Prozess momentan enthält.
+  int number_of_rand_max;			//Anzahl der Zufallszahlen, die ein Prozess enthalten kann.
   int* buf; 					//Ein Puffer für die Zufallszahlen für jeden Prozess
 
   
@@ -103,13 +116,13 @@ main (int argc, char** argv)
   successor = (size+rank+1) %size;		//Nachfolger bestimmen
   predecessor = (size+rank-1) %size;		//Vorgänger bestimmen
   number_of_rand = N % size <= rank ?  N / size : N / size + 1;//bestimmt die Anzahl der Zufallszahlen, die der Prozess erstellen soll
+  number_of_rand_max = N / size + 1;
 
-  buf = init(number_of_rand,rank);//ermittelt number_of_rand Zufallszahlen.
+  buf = init(number_of_rand,number_of_rand_max,rank);//ermittelt number_of_rand Zufallszahlen.
 
-  print_rand_array("Before",rank,number_of_rand,size,buf);
-// circle(buf);
-circle (buf,number_of_rand,rank,predecessor,successor,size);
-  print_rand_array("After",rank,number_of_rand,size,buf);
+  print_rand_array("Before",rank,number_of_rand,number_of_rand_max,size,buf);
+  buf = circle(buf,rank,predecessor,successor,size, number_of_rand_max);
+  print_rand_array("After",rank,number_of_rand,number_of_rand_max,size,buf);
   MPI_Finalize();
   return EXIT_SUCCESS;
 }
