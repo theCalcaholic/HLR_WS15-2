04e1e7f797527c749bd390535cccdd2e43478dda
theCalcaholic <tobias@knoeppler.net>
2015-12-09 14:42:17 +0100

    enabled debug output for max_residuum

--- 08-PDE/08-PDE/partdiff-par.c
+++ 08-PDE/08-PDE/partdiff-par.c
@@ -305,7 +305,7 @@ calculate2 (
 					maxresiduum_send = maxresiduum;
 					//printf("rank %d: local maxresiduum=%f\n", arguments->rank, maxresiduum);
 					MPI_Allreduce(&maxresiduum_send, &maxresiduum, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD); //maximiert maxresiduum auf alle Prozesse
-					if(arguments->rank == 0) printf("global maxresiduum=%f\n", maxresiduum);
+					if(rank == 0) printf("global maxresiduum=%f\n", maxresiduum);
         }
 
         Matrix_Out[i][j] = star;
