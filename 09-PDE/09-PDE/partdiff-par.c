2116d9361a7f34a9afbf7f8f043d5801a3ae7b9d
klauszi <klausz1993@hotmail.de>
2015-12-24 20:59:29 +0100

    copided displaymatrix2 by hlr insert mpi-stuff only jacobi works with process 0 gauß do nothing yet

--- 09-PDE/09-PDE/partdiff-par.c
+++ 09-PDE/09-PDE/partdiff-par.c
@@ -26,6 +26,7 @@
 #include <math.h>
 #include <malloc.h>
 #include <sys/time.h>
+#include <mpi.h>
 
 #include "partdiff-par.h"
 
@@ -368,6 +369,93 @@ DisplayMatrix (struct calculation_arguments* arguments, struct calculation_resul
 	fflush (stdout);
 }
 
+/**
+ * rank and size are the MPI rank and size, respectively.
+ * from and to denote the global(!) range of lines that this process is responsible for.
+ *
+ * Example with 9 matrix lines and 4 processes:
+ * - rank 0 is responsible for 1-2, rank 1 for 3-4, rank 2 for 5-6 and rank 3 for 7.
+ *   Lines 0 and 8 are not included because they are not calculated.
+ * - Each process stores two halo lines in its matrix (except for ranks 0 and 3 that only store one).
+ * - For instance: Rank 2 has four lines 0-3 but only calculates 1-2 because 0 and 3 are halo lines for other processes. It is responsible for (global) lines 5-6.
+ */
+static
+void
+DisplayMatrix2 (struct calculation_arguments* arguments, struct calculation_results* results, struct options* options, int rank, int size, int from, int to)
+{
+	//printf("Displaying - rank %d: from %d to %d\n", rank, from, to);
+	
+  int const elements = 8 * options->interlines + 9;
+
+  int x, y;
+  double** Matrix = arguments->Matrix[results->m];
+  MPI_Status status;
+
+  /* first line belongs to rank 0 */
+  if (rank == 0)
+    from--;
+
+  /* last line belongs to rank size - 1 */
+  if (rank + 1 == size)
+    to++;
+
+  if (rank == 0)
+    printf("Matrix:\n");
+
+  for (y = 0; y < 9; y++)
+  {
+    int line = y * (options->interlines + 1);
+
+		//printf("DM: Doing MPI stuff... line:%d\n", line);
+    if (rank == 0)
+    {
+      /* check whether this line belongs to rank 0 */
+      if (line < from || line > to)
+      {
+        /* use the tag to receive the lines in the correct order
+         * the line is stored in Matrix[0], because we do not need it anymore */
+        MPI_Recv(Matrix[0], elements, MPI_DOUBLE, MPI_ANY_SOURCE, 42 + y, MPI_COMM_WORLD, &status);
+      }
+    }
+    else
+    {
+      if (line >= from && line <= to)
+      {
+        /* if the line belongs to this process, send it to rank 0
+         * (line - from + 1) is used to calculate the correct local address */
+        MPI_Send(Matrix[line - from + 1], elements, MPI_DOUBLE, 0, 42 + y, MPI_COMM_WORLD);
+      }
+    }
+
+		//printf("MPI stuff finished\n");
+
+    if (rank == 0)
+    {
+      for (x = 0; x < 9; x++)
+      {
+        int col = x * (options->interlines + 1);
+
+        if (line >= from && line <= to)
+        {
+          /* this line belongs to rank 0 */
+          printf("%7.4f", Matrix[line][col]);
+        }
+        else
+        {
+          /* this line belongs to another rank and was received above */
+          printf("%7.4f", Matrix[0][col]);
+        }
+      }
+
+      printf("\n");
+    }
+  }
+
+	//printf("DisplayMatrix2 finished");
+
+  fflush(stdout);
+}
+
 /* ************************************************************************ */
 /*  main                                                                    */
 /* ************************************************************************ */
@@ -378,22 +466,47 @@ main (int argc, char** argv)
 	struct calculation_arguments arguments;
 	struct calculation_results results;
 
-	/* get parameters */
-	AskParams(&options, argc, argv);              /* ************************* */
+  int rank;             // Nummer des P
+  int size;             // Anzahl der P
 
-	initVariables(&arguments, &results, &options);           /* ******************************************* */
+  MPI_Init(&argc, &argv);                         // MPI initialisieren
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);           // Nummer des P holen 
+  MPI_Comm_size(MPI_COMM_WORLD, &size);           // Anzahl der P holen
 
-	allocateMatrices(&arguments);        /*  get and initialize variables and matrices  */
-	initMatrices(&arguments, &options);            /* ******************************************* */
+  if(rank == 0)
+  {
+    /* get parameters */
+    AskParams(&options, argc, argv);              
+  }
+  
+  //Der unschöne Weg! Broadcastet jede Variable in options.
+  MPI_Bcast(&(options.number),1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&(options.method),1,MPI_UINT64_T, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&(options.interlines),1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&(options.inf_func),1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&(options.termination),1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&(options.term_iteration),1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&(options.term_precision),1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  
+  initVariables(&arguments, &results, &options);           
 
-	gettimeofday(&start_time, NULL);                   /*  start timer         */
-	calculate(&arguments, &results, &options);                                      /*  solve the equation  */
-	gettimeofday(&comp_time, NULL);                   /*  stop timer          */
+  if ((options.method == METH_JACOBI) && (rank == 0))     // Bei nicht Jacobi einfach das Vorherige mit P 0 tun.
+  {
 
-	displayStatistics(&arguments, &results, &options);
-	DisplayMatrix(&arguments, &results, &options);
+    allocateMatrices(&arguments);             /*  get and initialize variables and matrices  */
+    initMatrices(&arguments, &options);          
 
-	freeMatrices(&arguments);                                       /*  free memory     */
+    gettimeofday(&start_time, NULL);                    /*  start timer         */
+    calculate(&arguments, &results, &options);          /*  solve the equation  */
+    gettimeofday(&comp_time, NULL);                     /*  stop timer          */
+
+    displayStatistics(&arguments, &results, &options);
+    DisplayMatrix(&arguments, &results, &options);
+
+    freeMatrices(&arguments);                           /*  free memory     */
+  }
+
+  MPI_Finalize();             //beendet MPI
 
 	return 0;
 }
